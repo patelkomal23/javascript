8be283eb78f3b9af32dd70f736b4631256a3fecb
patelkomal23 <patelkomal2332@gmail.com>
2025-01-28 18:03:07 +0530

    javascript

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("HELLO WPRLD!");
+
+document.write("<h1>How are you...?</h1>");
+
+// alert("Good Morning...");
+
+let title=document.getElementById("title");
+title.innerHTML ="Welcome to JavaScript...";
+
+let text=document.getElementById("text");
+text.innerText = "Lorem ipsum dolor sit amet consectetur";
+
+// let name = prompt("what is you name:-");
+// console.log("hello, "+name);
+
+// let user = "komal";
+// console.log(`Hello, ${user}!`);
+
+let p = document.createElement("p");
+p.textContent = "This is a new paragraph.";
+document.body.appendChild(p);
+
+let data = [{ name: "komal", age: 23 }, { name: "payal", age: 20 }];
+console.table(data);
+
