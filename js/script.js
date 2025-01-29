48de6db861348e630ef464a92480b19c375165a9
patelkomal23 <patelkomal2332@gmail.com>
2025-01-29 17:43:43 +0530

    variable

--- js/script.js
+++ js/script.js
@@ -1,25 +1,38 @@
+
+// 1
 console.log("HELLO WPRLD!");
 
-document.write("<h1>How are you...?</h1>");
+// 2
+document.write("<h1>HELLO CODER...</h1>");
 
+// 3
 // alert("Good Morning...");
 
+
+// 4. innerHTML
 let title=document.getElementById("title");
 title.innerHTML ="Welcome to JavaScript...";
 
+// 5. innerText / textContent
 let text=document.getElementById("text");
 text.innerText = "Lorem ipsum dolor sit amet consectetur";
 
+
+// 6. prompt()
 // let name = prompt("what is you name:-");
 // console.log("hello, "+name);
 
+// 7. Template Literals (${} in backticks)
 // let user = "komal";
 // console.log(`Hello, ${user}!`);
 
-let p = document.createElement("p");
+// 8. Appending to the DOM
+let p = document.createElement("p");  
 p.textContent = "This is a new paragraph......";
 document.body.appendChild(p);
 
+
+// 9. console.table()
 let data = [{ name: "komal", age: 23 }, { name: "payal", age: 20 }];
 console.table(data);
 
---
+++ var/js/script.js
@@ -0,0 +1,34 @@
+
+// Variables
+let age = 23;
+const pi=3.14;
+let name = "komal";
+let valid =false;
+let value =null;
+let obj = {
+    1: "one",
+    2: "two"
+}
+
+// console.log("Age is:- " +age);
+// console.log("value:- "+pi);
+// console.log("Name is:- "+name);
+
+console.log("typeof Age "+ typeof age);
+console.log("typeof pi " + typeof pi);
+console.log("tyrpeof "+typeof name)
+console.log("typeof valid " + typeof valid);
+console.log("typeof value:" + typeof value);
+console.log("typeof obj :" + typeof obj);
+
+
+
+// arithmetic operator
+let a=10;
+let b=5;
+
+console.log(a+b);
+console.log(a-b);
+console.log("Multiplication: "+ a*b);
+console.log("Division: "+ a/b);
+console.log("Exponetional:"+ a**2);
