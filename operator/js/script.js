a0324039e3bcb7301ea30b9e3f6361ce74981096
patelkomal23 <patelkomal2332@gmail.com>
2025-01-30 17:45:52 +0530

    operator

---
+++ operator/js/script.js
@@ -0,0 +1,56 @@
+
+// arithmetic operator
+let a=10;
+let b=5;
+
+console.log(a+b);
+console.log(a-b);
+console.log("Multiplication: "+ a*b);
+console.log("Division: "+ a/b);
+console.log("Exponetional:"+ a**2);
+
+p=200;
+q='200';
+
+console.log("p+q= "+ p+q);
+console.log("p-q= "+ p-q);
+
+
+
+//swaiping without 3 variable
+let c=20;
+let d=30;
+console.log("Before swaping c is:"+ c);
+console.log("Before swaping d is:"+ d);
+c=c+d;
+d=c-d;
+c=c-d;
+
+console.log("After swaping c is:"+ c);
+console.log("After swaping d is:"+ d);
+
+
+//swaping with third varaible
+let x=12;
+let y=11;
+console.log("Before swaping x is:"+ x);
+console.log("Before swaping y is:"+ y);
+let temp=x;
+x=y;
+y=temp;
+
+console.log("After swaping x is:"+ x);
+console.log("After swaping y is:"+ y);
+
+
+//greade calc
+// let marks = parseInt(prompt("Enter marks"));
+// let avg = marks; 
+// console.log("The average marks is: " + avg);
+
+// let grade = avg >= 80 ? 'A' : 
+//             avg >= 60 ? 'B' : 
+//             avg > 50 ? 'C' : 
+//             'Pass class';
+
+// console.log(grade);
