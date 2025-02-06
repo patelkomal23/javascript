6e915bbdaf2ff671e67d6a8a7446d18487f86457
patelkomal23 <patelkomal2332@gmail.com>
2025-02-06 17:40:07 +0530

    loop

---
+++ for_loop/script.js
@@ -0,0 +1,16 @@
+
+// let str= ' ';
+// for(let i=1;i<=5;i++){
+//     str += i + ' ,';
+// }
+
+// console.log(str);
+
+// let i=1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+    
+// }
+
+let i=
