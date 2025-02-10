045f463eb80089b824ac6827b602d57585510830
patelkomal23 <patelkomal2332@gmail.com>
2025-02-10 17:46:40 +0530

    fun

--- Loop-program/script.js
+++ Loop-program/script.js
@@ -1,4 +1,5 @@
 //prog-1 Print numbers 1 to 10 using for loop
+
 // let str='';
 // for(let i=1;i<=10;i++){
 //     str += i+' , ';
@@ -6,6 +7,7 @@
 // console.log(str);
 
 //prog-2 Print numbers 1 to 10 using while loop
+
 // let str='';
 // let i=1;
 // while(i<=10){
@@ -15,6 +17,7 @@
 // console.log(str);
 
 //prog-3 Print numbers 1 to 10 using do-while loop
+
 // let i=1;
 // do{
 //     console.log(i);
@@ -22,6 +25,7 @@
 // }while(i<=10);
 
 //prog-4 Sum of first 10 natural numbers (for loop)
+
 // let ans=0;
 // for(i=1;i<=10;i++){
 //     ans+=i;
@@ -30,6 +34,8 @@
 
 
 //prog-5  Sum of first 10 natural numbers (while loop)
+
+
 // let ans=0;
 // let i=1;
 // while(i<=10){
@@ -39,6 +45,7 @@
 // console.log(Answer:${ans});
 
 //prog-6 Sum of first 10 natural numbers (do-while loop)
+
 // let ans = 0;
 // let i = 1;
 // do {
@@ -48,6 +55,7 @@
 // console.log(Answer:${ans});
 
 //prog-7 Print even numbers from 1 to 20
+
 // let str='';
 // for(let i=2;i<=20;i+=2){
 //     str+=i +',';
@@ -55,6 +63,7 @@
 // console.log(str);
 
 //prog-8  Print odd numbers from 1 to 20
+
 // let str='';
 // for(let i=1;i<=20;i+=2){
 //     str+=i +',';
@@ -62,11 +71,13 @@
 // console.log(str);
 
 //prog-9 Print multiplication table of 5
+
 // for(let i=1;i<=10;i++){
 //     console.log("5*"+i + "=" + 5*i);
 // }
 
 //prog-10 Reverse countdown from 10 to 1
+
 // let i=10;
 // let str='';
 // while(i>=1){
@@ -75,6 +86,7 @@
 // }console.log(str);
 
 //prog-11 Factorial of a number (for loop)
+
 // let f=1;
 // let number=5;
 // for(let i=1;i<=number;i++){
@@ -83,6 +95,7 @@
 // console.log(Factorial : ${f});
 
 //prog-12 Find the sum of digits of a number
+
 // let number = 3053, sum = 0;
 // while (number>0) {
 //  sum += number % 10;
@@ -91,6 +104,7 @@
 // console.log(Sum of digits: ${sum});
 
 //prog-13 Reverse a number
+
 // let number = 3053, reverse = 0;
 // while (number > 0) {
 //  reverse = reverse * 10 + (number % 10);
