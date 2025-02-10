10c65bd98d7e08be163b1a1cec50f36c436b5c28
patelkomal23 <patelkomal2332@gmail.com>
2025-02-10 12:54:07 +0530

    done

---
+++ Loop-program/script.js
@@ -0,0 +1,37 @@
+// 1. Print numbers 1 to 10 using for loop
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// 2. Print numbers 1 to 10 using while loop
+//  let i = 1;
+//  while(i<=10){
+//     console.log(i);
+//     i ++;
+    
+//  }
+// 3. Print numbers 1 to 10 using do-while loop
+// let i=1;
+// do {
+//     console.log(i);
+//     i++;
+    
+// } while (i<=10);
+
+// 4. Sum of first 10 natural numbers (for loop)
+
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//  sum += i;
+// }
+// console.log("Sum:", sum);
+
+// 5. Sum of first 10 natural numbers (while loop)
+
+
+let i=1, sum=0;
+while(i<=10){
+    sum+=i;
+    i++;
+    
+}
+console.log("Sum:",sum);
