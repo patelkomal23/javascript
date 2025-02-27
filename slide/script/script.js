962dcb8b07217fa65dff3f34fb11a30fc791d80e
patelkomal23 <patelkomal2332@gmail.com>
2025-02-27 18:09:46 +0530

    done

--- slide/script/script.js
+++ slide/script/script.js
@@ -1,3 +1,17 @@
-let slideIndex=0;
+let slideIndex = 0;
 let sildeItem = document.querySelectorAll('.slider-item');
 console.log(sildeItem[0]);
+
+let currenSlide = (n) => {
+    displaySlide(slideIndex+=n);
+    console.log(slideIndex);
+    
+}
+let displaySlide = (slideIndex) => {
+    for (let i = 0; i < sildeItem.length; i++) {
+        sildeItem[i].classList.remove('active');
+    }
+    sildeItem[slideIndex].classList.add('active');
+
+
+}
