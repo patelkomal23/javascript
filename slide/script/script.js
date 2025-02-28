25038cafd5766793cceb3ad0e8e6e6c1fe6a1084
patelkomal23 <patelkomal2332@gmail.com>
2025-02-28 15:46:59 +0530

    done

--- slide/script/script.js
+++ slide/script/script.js
@@ -1,17 +1,27 @@
-let slideIndex = 0;
-let sildeItem = document.querySelectorAll('.slider-item');
-console.log(sildeItem[0]);
 
-let currenSlide = (n) => {
-    displaySlide(slideIndex+=n);
-    console.log(slideIndex);
-    
-}
-let displaySlide = (slideIndex) => {
-    for (let i = 0; i < sildeItem.length; i++) {
-        sildeItem[i].classList.remove('active');
-    }
-    sildeItem[slideIndex].classList.add('active');
+let slideIndex = 1;
+//   console.log(slideItem[0]);
 
+let currSlide = (n) => {
+  displaySlide((slideIndex += n));
+};
 
-}
+let displaySlide = (n) => {
+  let slideItem = document.querySelectorAll(".slider-item");
+  console.log(n);
+  console.log(slideIndex);
+
+  if (n > slideItem.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slideItem.length;
+  }
+
+  for (let i = 0; i < slideItem.length; i++) {
+    slideItem[i].classList.remove("active");
+  }
+  slideItem[slideIndex - 1].classList.add("active");
+};
+
+displaySlide(slideIndex);
