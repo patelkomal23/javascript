8d4bb8c58a2c172caffa79d014e88d58733da9b8
patelkomal23 <patelkomal2332@gmail.com>
2025-03-01 22:11:35 +0530

    donee

--- slide/script/script.js
+++ slide/script/script.js
@@ -1,42 +1,35 @@
-
 let slideIndex = 1;
-//   console.log(slideItem[0]);
-let dots = document.querySelectorAll(".dot");
-let setSlide =(n)=>{
-  slideIndex=n;
-  displaySlide(slideIndex);
+let slideItem = document.querySelectorAll(".slider-item");
+let currSlideInd = document.getElementById("current-slide");
+let totalSlideInd = document.getElementById("total-slide");
+let dots=document.querySelectorAll(".dot");
 
-};
-
+totalSlideInd.textContent = slideItem.length;
 
 let currSlide = (n) => {
-  displaySlide((slideIndex += n));
+    displaySlide((slideIndex += n));
 };
 
 let displaySlide = (n) => {
-  let slideItem = document.querySelectorAll(".slider-item");
-  console.log(n);
-  console.log(slideIndex);
+    if (n > slideItem.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slideItem.length;
+    }
 
-  if (n > slideItem.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slideItem.length;
-  }
+    slideItem.forEach((slide) => slide.classList.remove("active"));
+    dots.forEach((dot)=>dot.classList.remove("active"));
 
-  for (let i = 0; i < slideItem.length; i++) {
-    slideItem[i].classList.remove("active");
-  }
-  slideItem[slideIndex - 1].classList.add("active");
+    slideItem[slideIndex - 1].classList.add("active");
+    dots[slideIndex-1].classList.add("active");
 
-dots.forEach((dot) => dot.classList.remove("active-dot"));
-dots[slideIndex - 1].classList.add("active-dot");
+    currSlideInd.textContent = slideIndex;
 };
 
 
+displaySlide(slideIndex);
+
 setInterval(() => {
-      currSlide(1);
-  
-  }, 5000);
-  
+    currSlide(1);
+}, 2000);
