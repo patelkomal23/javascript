5d1d8a3458b410de2f715a7e89043da59d762aa7
patelkomal23 <patelkomal2332@gmail.com>
2025-03-13 17:20:22 +0530

    done

---
+++ OOPs/Constructors/script.js
@@ -0,0 +1,13 @@
+//deafult const
+class Demo{
+    constructor(){
+        console.log("This is from constructor.");
+    }
+    print(){
+        console.log("This is from Demo.");
+    }
+}
+let obj=new Demo();
+obj.print();
+
+//
