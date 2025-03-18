b96b1d37038cff8d84d57f6a5bafdacaaa936145
patelkomal23 <patelkomal2332@gmail.com>
2025-03-18 17:18:21 +0530

    done

--- OOPs/Constructors/script.js
+++ OOPs/Constructors/script.js
@@ -10,4 +10,4 @@ class Demo{
 let obj=new Demo();
 obj.print();
 
-//
+//                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
---
+++ OOPs/inheritance/demo.js
@@ -0,0 +1,15 @@
+
+class Employee{
+    constructor(name,age){
+        console.log(this.name = name);
+        console.log(this.age = age);
+    }
+}
+
+class Manager extends Employee{
+    constructor(name,age){
+        super(name,age)
+    }
+}
+
+let obj = new Manager("Akbar",27);
