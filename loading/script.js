ebdfe29136c1fc6eb83eb71fdb645f84e30d2ba5
patelkomal23 <patelkomal2332@gmail.com>
2025-03-20 17:05:20 +0530

    done

---
+++ loading/script.js
@@ -0,0 +1,6 @@
+window.onload =function(){
+    setTimeout(()=>{
+        document.querySelector(".loader").style.display = "none";
+        document.querySelector(".content").style.display ="block";
+    },2000)
+}
