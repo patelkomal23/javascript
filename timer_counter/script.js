709d7f406a400d2eeaa21cbf816dd4230f00f585
patelkomal23 <patelkomal2332@gmail.com>
2025-03-24 17:38:32 +0530

    done

---
+++ timer_counter/script.js
@@ -0,0 +1,16 @@
+let time = document.getElementById('time').textContent;
+
+let id;
+
+const startTimer = () => {
+    id = setInterval(() => {
+        if (time == 0) {
+            alert("Time out!")
+            clearInterval(id);
+            return;
+        }
+         time--;
+         document.getElementById('time').textContent= time>9?time: '0'+time
+
+    })
+}
